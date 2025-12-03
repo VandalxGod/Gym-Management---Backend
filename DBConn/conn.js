e9dadbaab372bb8faf629f0ea07403e53fd6c38d
VandalxGod <sumit.tutu2506@gmail.com>
2025-12-03 13:20:24 +0530

    Fix db

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://sumittutu2506_db_user:ddvbbm6po5CEdUV3@gym.vhgxlrb.mongodb.net/')
+mongoose.connect('mongodb+srv://sumittutu2506_db_user:hlH5fi48Ps0aFrEF@gym.vhgxlrb.mongodb.net/')
   .then(() => console.log('DB connection successful!'))
   .catch(err => {
     console.log(err)
