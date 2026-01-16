af44fa423b346e3ea5f29b225ab8d819b57797ae
VandalxGod <sumit.tutu2506@gmail.com>
2026-01-16 14:14:57 +0530

    Database connect atlas

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
+
+mongoose.connect(process.env.atlas_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
